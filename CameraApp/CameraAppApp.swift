1ba512cf1bfd708b9087e61babe5a1d894b6814d
My Tran <mtran2424@gmail.com>
2023-11-02 11:41:50 -0400

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by My Tran on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
